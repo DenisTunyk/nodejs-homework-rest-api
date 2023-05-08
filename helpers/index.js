39d96030ff44be4e7ccf4c74e305cd7923ff35d5
DenisTunyk <tundenis43@gmail.com>
2023-05-08 11:37:15 +0300

    homework_6

--- helpers/index.js
+++ helpers/index.js
@@ -1,8 +1,10 @@
 const HttpError = require("./HttpError");
 const ctrlWrapper = require("./ctrlWrapper");
 const handleMongoosError = require("./handleMongoosError");
+const sendEmail = require("./sendEmail");
 module.exports = {
   HttpError,
   ctrlWrapper,
   handleMongoosError,
+  sendEmail,
 };
--- routes/api/auth.js
+++ routes/api/auth.js
@@ -9,7 +9,16 @@ const {
 } = require("../../middleware");
 const { schemas } = require("../../models/user");
 
+//signup
 router.post("/register", validateBody(schemas.registerSchema), ctrl.register);
+router.get("/verify/:verificationCode", ctrl.verifyEmail);
+router.post(
+  "/verify",
+  validateBody(schemas.emailSchema),
+  ctrl.resendVerifyEmail
+);
+
+//signin
 router.post("/login", validateBody(schemas.loginSchema), ctrl.login);
 router.get("/current", authenticate, ctrl.getCurrent);
 router.post("/logout", authenticate, ctrl.logout);
